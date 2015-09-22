208a0e3888d9ff62f237d00c8445b704b6f122aa
g0294 <g0294@edaworld.com.tw>
2015-09-22 10:26:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/g0294/androidtutorial/HelloWorldActivity.java
@@ -0,0 +1,16 @@
+package com.example.g0294.androidtutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class HelloWorldActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+    }
+
+}
