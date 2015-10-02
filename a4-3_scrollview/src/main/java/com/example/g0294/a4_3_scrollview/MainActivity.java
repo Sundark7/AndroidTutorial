fb9d58e9dab47655cbd23df08054eeb193d2fde7
g0294 <g0294@edaworld.com.tw>
2015-10-02 15:51:33 +0800

    A4-3 Scroll View

--- a4-3_scrollview/src/main/java/com/example/g0294/a4_3_scrollview/MainActivity.java
+++ a4-3_scrollview/src/main/java/com/example/g0294/a4_3_scrollview/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.g0294.a4_3_scrollview;
 import android.app.Activity;
 import android.os.Bundle;
 
-/**
- * Created by g0294 on 2015/10/2.
- */
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
