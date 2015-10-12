c6c49e58435f349e42cd445d018dad042db85402
g0294 <g0294@edaworld.com.tw>
2015-10-12 15:23:20 +0800

    Change Text View Example

--- a4-1_inputcontrols/src/main/java/com/example/g0294/a4_1_inputcontrols/InputControlActivity.java
+++ a4-1_inputcontrols/src/main/java/com/example/g0294/a4_1_inputcontrols/InputControlActivity.java
@@ -7,75 +7,76 @@ import android.view.View;
 import android.widget.Button;
 
 public class InputControlActivity extends Activity {
-    private Button btn_textView, btn_editText, btn_imageView, btn_button, btn_checkBox, btn_radioButton, btn_datetimePicker, btn_progressBar, btn_seekBar;
+  private Button btn_textView, btn_editText, btn_imageView, btn_button, btn_checkBox,
+      btn_radioButton, btn_datetimePicker, btn_progressBar, btn_seekBar;
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.inputcontrol_layout);
+    SelectActivity listener = new SelectActivity();
+    btn_textView = (Button) findViewById(R.id.btn_textView);
+    btn_textView.setOnClickListener(listener);
+    btn_editText = (Button) findViewById(R.id.btn_editText);
+    btn_editText.setOnClickListener(listener);
+    btn_imageView = (Button) findViewById(R.id.btn_imageView);
+    btn_imageView.setOnClickListener(listener);
+    btn_button = (Button) findViewById(R.id.btn_button);
+    btn_button.setOnClickListener(listener);
+    btn_checkBox = (Button) findViewById(R.id.btn_checkBox);
+    btn_checkBox.setOnClickListener(listener);
+    btn_radioButton = (Button) findViewById(R.id.btn_radioButton);
+    btn_radioButton.setOnClickListener(listener);
+    btn_datetimePicker = (Button) findViewById(R.id.btn_datetimePicker);
+    btn_datetimePicker.setOnClickListener(listener);
+    btn_progressBar = (Button) findViewById(R.id.btn_progressBar);
+    btn_progressBar.setOnClickListener(listener);
+    btn_seekBar = (Button) findViewById(R.id.btn_seekBar);
+    btn_seekBar.setOnClickListener(listener);
+  }
+
+  class SelectActivity implements View.OnClickListener {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.inputcontrol_layout);
-        SelectActivity listener = new SelectActivity();
-        btn_textView = (Button) findViewById(R.id.btn_textView);
-        btn_textView.setOnClickListener(listener);
-        btn_editText = (Button) findViewById(R.id.btn_editText);
-        btn_editText.setOnClickListener(listener);
-        btn_imageView = (Button) findViewById(R.id.btn_imageView);
-        btn_imageView.setOnClickListener(listener);
-        btn_button = (Button) findViewById(R.id.btn_button);
-        btn_button.setOnClickListener(listener);
-        btn_checkBox = (Button) findViewById(R.id.btn_checkBox);
-        btn_checkBox.setOnClickListener(listener);
-        btn_radioButton = (Button) findViewById(R.id.btn_radioButton);
-        btn_radioButton.setOnClickListener(listener);
-        btn_datetimePicker = (Button) findViewById(R.id.btn_datetimePicker);
-        btn_datetimePicker.setOnClickListener(listener);
-        btn_progressBar = (Button) findViewById(R.id.btn_progressBar);
-        btn_progressBar.setOnClickListener(listener);
-        btn_seekBar = (Button) findViewById(R.id.btn_seekBar);
-        btn_seekBar.setOnClickListener(listener);
+    public void onClick(View view) {
+      Intent intent = new Intent();
+      switch (view.getId()) {
+        case R.id.btn_textView:
+          intent.setClass(getApplicationContext(), TextViewActivity.class);
+          startActivity(intent);
+          break;
+        case R.id.btn_editText:
+          intent.setClass(getApplicationContext(), EditTextActivity.class);
+          startActivity(intent);
+          break;
+        case R.id.btn_imageView:
+          intent.setClass(getApplicationContext(), ImageViewActivity.class);
+          startActivity(intent);
+          break;
+        case R.id.btn_button:
+          intent.setClass(getApplicationContext(), ButtonActivity.class);
+          startActivity(intent);
+          break;
+        case R.id.btn_checkBox:
+          intent.setClass(getApplicationContext(), CheckBoxActivity.class);
+          startActivity(intent);
+          break;
+        case R.id.btn_radioButton:
+          intent.setClass(getApplicationContext(), RadioButtonActivity.class);
+          startActivity(intent);
+          break;
+        case R.id.btn_datetimePicker:
+          intent.setClass(getApplicationContext(), DateTimePickerActivity.class);
+          startActivity(intent);
+          break;
+        case R.id.btn_progressBar:
+          intent.setClass(getApplicationContext(), ProgressbarActivity.class);
+          startActivity(intent);
+          break;
+        case R.id.btn_seekBar:
+          intent.setClass(getApplicationContext(), SeekRatingBarActivity.class);
+          startActivity(intent);
+          break;
+      }
     }
-
-    class SelectActivity implements View.OnClickListener {
-        @Override
-        public void onClick(View view) {
-            Intent intent = new Intent();
-            switch (view.getId()) {
-                case R.id.btn_textView:
-                    intent.setClass(getApplicationContext(), TextViewActivity.class);
-                    startActivity(intent);
-                    break;
-                case R.id.btn_editText:
-                    intent.setClass(getApplicationContext(), EditTextActivity.class);
-                    startActivity(intent);
-                    break;
-                case R.id.btn_imageView:
-                    intent.setClass(getApplicationContext(), ImageViewActivity.class);
-                    startActivity(intent);
-                    break;
-                case R.id.btn_button:
-                    intent.setClass(getApplicationContext(), ButtonActivity.class);
-                    startActivity(intent);
-                    break;
-                case R.id.btn_checkBox:
-                    intent.setClass(getApplicationContext(), CheckBoxActivity.class);
-                    startActivity(intent);
-                    break;
-                case R.id.btn_radioButton:
-                    intent.setClass(getApplicationContext(), RadioButtonActivity.class);
-                    startActivity(intent);
-                    break;
-                case R.id.btn_datetimePicker:
-                    intent.setClass(getApplicationContext(), DateTimePickerActivity.class);
-                    startActivity(intent);
-                    break;
-                case R.id.btn_progressBar:
-                    intent.setClass(getApplicationContext(), ProgressbarActivity.class);
-                    startActivity(intent);
-                    break;
-                case R.id.btn_seekBar:
-                    intent.setClass(getApplicationContext(), SeekRatingBarActivity.class);
-                    startActivity(intent);
-                    break;
-            }
-        }
-    }
+  }
 }
--- a4-1_inputcontrols/src/main/java/com/example/g0294/a4_1_inputcontrols/TextViewActivity.java
+++ a4-1_inputcontrols/src/main/java/com/example/g0294/a4_1_inputcontrols/TextViewActivity.java
@@ -6,13 +6,15 @@ import android.view.View;
 import android.widget.TextView;
 
 public class TextViewActivity extends Activity {
-    private TextView textView;
-
+    private TextView textView, tvAge;
+    protected String str;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.text_view_layout);
         textView = (TextView) findViewById(R.id.tv_marquee);
+        tvAge = (TextView)findViewById(R.id.tvAge);
+
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -20,5 +22,7 @@ public class TextViewActivity extends Activity {
                 textView.setFocusableInTouchMode(true);
             }
         });
+        str = getResources().getString(R.string.your_age);
+        tvAge.setText(String.format(str, "王曉明", 1990, (2015-1990)));
     }
 }
