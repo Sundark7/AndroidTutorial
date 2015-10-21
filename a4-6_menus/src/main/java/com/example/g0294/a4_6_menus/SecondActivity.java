6d607ddf9d3dfe6865873c43996326964808a2b8
g0294 <g0294@edaworld.com.tw>
2015-10-21 16:37:24 +0800

    Change Menus Example

--- a4-6_menus/src/main/java/com/example/g0294/a4_6_menus/SecondActivity.java
+++ a4-6_menus/src/main/java/com/example/g0294/a4_6_menus/SecondActivity.java
@@ -1,17 +1,21 @@
 package com.example.g0294.a4_6_menus;
 
-import android.app.Activity;
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class SecondActivity extends Activity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        // getActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar mActionBar = getActionBar();
+        if (mActionBar!=null)
+            mActionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -23,16 +27,14 @@ public class SecondActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        // noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
